2617aca47082244de4a3f4512aadcc180dded021
zombieyang <zombieyang@tencent.com>
2016-08-08 11:09:08 +0800

    compat browser

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-var OPTION = {
-	's': [1, 1e-9],
-	'ms': [1e3, 1e-6],
-	'us': [1e6, 1e-3],
-	'ns': [1e9, 1]
-};
+if (typeof process == 'undefined') {
 
-module.exports = function (hrtime, option) {
-	var time = hrtime ? process.hrtime(hrtime) : process.hrtime();
-	if (option && option in OPTION) {
-		time = time[0] * OPTION[option][0] + time[1] * OPTION[option][1]
-	}
-	return time
-};
+    module.exports = function (date, option) {
+        var time = date ? (new Date).getTime() - date : (new Date).getTime();
+        switch (option) {
+            case 's':
+                return Math.floor(time / 1000);
+            case 'us':
+                return time * 1000;
+            case 'ns':
+                return time * 1000000;
+            default:
+                return time
+        }
+    };
+} else {
+    var OPTION = {
+        's': [1, 1e-9],
+        'ms': [1e3, 1e-6],
+        'us': [1e6, 1e-3],
+        'ns': [1e9, 1]
+    };
+    module.exports = function (hrtime, option) {
+        var time = hrtime ? process.hrtime(hrtime) : process.hrtime();
+        if (option && option in OPTION) {
+            time = time[0] * OPTION[option][0] + time[1] * OPTION[option][1]
+        }
+        return time
+    };
+}
